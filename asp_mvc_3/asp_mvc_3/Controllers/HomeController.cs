22b351c99f00a0fb7f0594e1b95646971ce9ec2d
Religi Nurhidayah Sakthi <religi_sakthi_24rpl@student.smktelkom-mlg.sch.id>
2017-02-12 22:27:25 +0700

    Modul 1 ASP finish

---
+++ asp_mvc_3/asp_mvc_3/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace asp_mvc_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome() { return View(); }
+    }
+}
